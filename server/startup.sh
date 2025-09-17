970dd6b9cfc76659df8906c9461d914ad521026d
PhilHenning <phillyp.henning@gmail.com>
2025-09-17 14:44:36 -0600

    updating startup command

--- server/startup.sh
+++ server/startup.sh
@@ -6,4 +6,6 @@ OUTPUT_PATH="$TEMPORAL_CONFIG_PATH/$TEMPORAL_CONFIG_FILENAME.yaml"
 # Render the template
 gomplate -f "$TEMPORAL_CONFIG_TEMPLATE_PATH" -o "$OUTPUT_PATH"
 
-echo "Rendered Temporal config to: $OUTPUT_PATH.yaml"
+echo "Rendered Temporal config to: $OUTPUT_PATH"
+
+/app/temporal-auth-server

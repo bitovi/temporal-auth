d2a306ee847571d927c70614b6f292e18e48effd
PhilHenning <phillyp.henning@gmail.com>
2025-09-17 15:20:49 -0600

    Adding POD_IP explicted + updating go.mod Temporal server package

--- server/startup.sh
+++ server/startup.sh
@@ -3,9 +3,14 @@ set -eu
 
 OUTPUT_PATH="$TEMPORAL_CONFIG_PATH/$TEMPORAL_CONFIG_FILENAME.yaml"
 
+POD_IP=$(hostname -i)
+
 # Render the template
 gomplate -f "$TEMPORAL_CONFIG_TEMPLATE_PATH" -o "$OUTPUT_PATH"
 
 echo "Rendered Temporal config to: $OUTPUT_PATH"
+echo "----- Config Start -----"
+cat "$OUTPUT_PATH"
+echo "----- Config End -----"
 
 /app/temporal-auth-server
